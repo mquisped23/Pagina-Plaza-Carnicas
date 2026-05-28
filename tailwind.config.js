2be627cbf164dd09b07739d1aec7e2c63354d1a1
mquisped23 <mquisped23@autonoma.edu.pe>
2026-05-27 19:33:20 -0500

    agregar tailwind cli

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.html",
+    "./components/**/*.html",
+    "./js/**/*.js"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          wine: "#5E0602",
+          wineDark: "#3B0301",
+          wineHover: "#630804",
+          gold: "#EAB237",
+          goldHover: "#F3C65A",
+          dark: "#101820",
+          black: "#070707",
+          cream: "#F8F5EF",
+          gray: "#6B7280",
+          white: "#FFFFFF"
+        }
+      },
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+        heading: ["Playfair Display", "serif"]
+      },
+      backgroundImage: {
+        texture: "url('https://www.transparenttextures.com/patterns/dark-matter.png')"
+      }
+    }
+  },
+  plugins: []
+}
